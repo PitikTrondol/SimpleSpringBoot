755dede1d61a2fe3aa9add847b3140e1265ba877
Afriandi <emailafriandi@gmail.com>
2024-10-13 15:18:30 +0700

    [Auth] Add data class

--- src/main/kotlin/anridiaf/playground/java/mark1/auth/User.kt
+++ src/main/kotlin/anridiaf/playground/java/mark1/auth/User.kt
@@ -1,3 +1,26 @@
 package anridiaf.playground.java.mark1.auth
 
-data class User()
+import jakarta.persistence.*
+
+@Entity
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    val id: Int,
+
+    @Column(name = "full_name")
+    val name: String,
+
+    @Column(name = "email")
+    val email: String,
+
+    @Column(name = "password")
+    val password: String,
+
+    @Column(name = "created_at")
+    val createdAt: Long,
+
+    @Column(name = "updated_at")
+    val updatedAt: Long,
+)
